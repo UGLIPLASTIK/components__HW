b3cf6a7b26ea941d5ba3f7db0430250f7e943f95
Dmitry.Kononets <d.cononec@gmail.com>
2023-12-04 00:15:06 +0300

    props-ts-task2 готова

--- props-ts-task2/src/App.tsx
+++ props-ts-task2/src/App.tsx
@@ -3,7 +3,6 @@ import data from './data/etsy.json'
 import Listing from './components/Listing'
 
 function App() {
-  console.log(data)
   return (
     <Listing item={data}/>
   )
--- props-ts-task2/src/components/Listing.tsx
+++ props-ts-task2/src/components/Listing.tsx
@@ -2,33 +2,55 @@ interface Items {
   item: {
     listing_id: number,
     url: string,
-    MainImage?: {
+    MainImage: {
       url_570xN: string,
     },
     title: string,
     currency_code: string,
     price: string,
     quantity: number,
+    state?: string,
   }[];
 }
 
 const Listing = ({ item }: Items) => {
+
+  function cutTitle(str: string): string | undefined {
+    if (str.length > 50) 
+    return str.substring(0, 49)+'...'
+    else return str
+  }
+
+  function setCurrency(code: string, value: string): string | undefined {
+    if (code == 'USD') return '$' + value
+    else if (code == 'EUR') return '€'+ value
+    else return value + ' ' + code
+  }
+
+  function setQuantity(quantity: number): string | undefined{
+    if (quantity <= 10) return 'level-low'
+    else if (quantity <= 20) return 'level-medium'
+    else return 'level-high'
+  }
+
   return (
     <div className="item-list">
-      {item.map(item => <div className="item" key={item.listing_id}>
-        <div className="item-image">
-          <a href="#">
-            <img src={item.MainImage?.url_570xN} alt={item.title} />
-          </a>
-        </div>
-        <div className="item-details">
-          <p className="item-title">{item.title}</p>
-          <p className="item-price">{item.price}</p>
-          <p className="item-quantity level-medium">{item.quantity}</p>
-        </div>
-      </div>)}
+      {item.map(item => {
+        if (item.state == 'active') return (
+          <div className="item" key={item.listing_id}>
+            <div className="item-image">
+              <a href="#">
+                <img src={item.MainImage.url_570xN} alt={item.title} />
+              </a>
+            </div>
+            <div className="item-details">
+              <p className="item-title">{cutTitle(item.title)}</p>
+              <p className="item-price">{setCurrency(item.currency_code, item.price)}</p>
+              <p className={`item-quantity ${setQuantity(item.quantity)}`}>{item.quantity} left</p>
+            </div>
+          </div>
+          )})}
     </div>
-   
   )
 }
 
